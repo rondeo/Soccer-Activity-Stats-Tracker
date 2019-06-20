256b70d3fd085f7bc005378ab02709977c577ebf
Coriano Harris <corianoharris@gmail.com>
2019-06-20 14:09:29 -0500

    Change the activeClassName to all lowercase per React docs

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import HomeIcon from 'react-icons/lib/fa/home'
-import AddDayIcon from 'react-icons/lib/fa/calendar-plus-o'
-import ListDaysIcon from 'react-icons/lib/fa/table'
-import '../stylesheets/menu.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import HomeIcon from "react-icons/lib/fa/home";
+import AddDayIcon from "react-icons/lib/fa/calendar-plus-o";
+import ListDaysIcon from "react-icons/lib/fa/table";
+import "../stylesheets/menu.css";
 
 export const Menu = () => (
-		<nav className="menu">
-			<Link to="/" activeClassName="selected">
-				<HomeIcon className="icon"/>
-			</Link>
-			<Link to="/add-day" activeClassName="selected">
-				<AddDayIcon className="icon" />
-			</Link>
-			<Link to="/list-days" activeClassName="selected">
-				<ListDaysIcon  className="icon"/>
-			</Link>
-		</nav>
-	)
+  <nav className="menu">
+    <Link to="/" activeclassname="selected">
+      <HomeIcon className="icon" />
+    </Link>
+    <Link to="/add-day" activeclassname="selected">
+      <AddDayIcon className="icon" />
+    </Link>
+    <Link to="/list-days" activeclassname="selected">
+      <ListDaysIcon className="icon" />
+    </Link>
+  </nav>
+);
